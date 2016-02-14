c284b38b3666757bb3a503a0e14932011633464e
nirmalks <kagosekar@gmail.com>
2016-02-14 23:51:06 +0530

    Added the default menu action bar(commented out previously)

--- app/src/main/java/com/example/nirmal/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/nirmal/myappportfolio/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-    /*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -41,5 +41,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    */
+    
 }
