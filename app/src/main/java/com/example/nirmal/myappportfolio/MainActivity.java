fa60d5845fb69c1491ea1f786365ed14a479a993
nirmalks <kagosekar@gmail.com>
2016-02-16 22:57:55 +0530

    Removed the code for the default action menu bar(previously commented out)

--- app/src/main/java/com/example/nirmal/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/nirmal/myappportfolio/MainActivity.java
@@ -45,27 +45,5 @@ public class MainActivity extends AppCompatActivity {
     public void capstoneClick(View view) {
         Toast.makeText(this,"This button will launch my Capstone: My Own App",Toast.LENGTH_SHORT).show();
     }
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
